e762856b8c87238beb4af9bdce77b158d77002bc
Viktor Volkov <vvvolkov@avito.ru>
2021-04-09 13:52:06 +0300

    add solution for 6-module 1-task

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -1,17 +1,29 @@
 const path = require('path');
 const Koa = require('koa');
 const app = new Koa();
+const EventEmitter = require('events');
 
 app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const messageEmitter = new EventEmitter();
 
-router.get('/subscribe', async (ctx, next) => {
+router.get('/subscribe', async (ctx) => {
+  ctx.body = await new Promise((resolve) => {
+    messageEmitter.once('message', (message) => {
+      resolve(message);
+    });
+  });
 });
 
-router.post('/publish', async (ctx, next) => {
+router.post('/publish', async (ctx) => {
+  const message = ctx.request.body.message;
+  if (message) {
+    messageEmitter.emit('message', ctx.request.body.message);
+  }
+  ctx.body = '';
 });
 
 app.use(router.routes());
